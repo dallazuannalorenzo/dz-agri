ac11456ecf6ce20d9fe1124cd31434b8a98ba689
Lorenzo <dallazuannalorenzo@gmail.com>
2022-05-26 10:50:24 +0200

    try to authenticate

--- index.js
+++ index.js
@@ -8,27 +8,7 @@ const pool = new Pool({
     rejectUnauthorized: false
   }
 });
-/**new */
-const { auth } = require('express-openid-connect');
 
-const config = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: '5e9eb80d79539383fc73eb7da30a606d8a46c4f8de761f56485052f421911abf',
-  baseURL: 'https://dz-agri.herokuapp.com',
-  clientID: 'jd9sKpFSn7cCMjZLtiWA7LDoHD8PF3rb',
-  issuerBaseURL: 'https://dev-ylukm6pm.us.auth0.com'
-};
-
-// auth router attaches /login, /logout, and /callback routes to the baseURL
-app.use(auth(config));
-
-// req.isAuthenticated is provided from the auth router
-app.get('/', (req, res) => {
-  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-});
-
-/**end new */
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
